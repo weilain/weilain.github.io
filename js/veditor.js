fb1b985498fd29906264d8d4c5fab7728d2e98fe
谌壮壮 <shenzhuang@aliyun.com>
2020-07-29 11:44:41 +0800

    Site updated: 2020-07-29 11:44:41

---
+++ js/veditor.js
@@ -0,0 +1,7 @@
+//可以与 valine 的配置 js 文件放一起，或放置到公共 js 文件中
+$("#veditor").focus(function(){$(this).css("background-image","none");});
+$("#veditor").blur(function(){
+      $(this).css("background-image","url(https://cdn.jsdelivr.net/gh/drew233/cdn/20200409110727.webp)");
+   }
+});
+

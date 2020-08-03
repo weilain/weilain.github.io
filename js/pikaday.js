199c63ba0259a28f09569627c5e46b8e18894dda
谌壮壮 <shenzhuang@aliyun.com>
2020-08-03 20:51:32 +0800

    Site updated: 2020-08-03 20:51:32

--- js/pikaday.js
+++ js/pikaday.js
@@ -22,7 +22,7 @@
       return [31, isLeapYear(year) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month];
     },
     Pikaday = function (opts) {
-      var data = '{"2018":{"11":{"22":true}},"2020":{"6":{"23":true,"24":true,"26":true,"27":true,"28":true,"29":true,"30":true,"31":true}}}';
+      var data = '{"2018":{"11":{"22":true}},"2020":{"6":{"23":true,"24":true,"26":true,"27":true,"28":true,"29":true,"30":true,"31":true},"7":{"3":true}}}';
       this.data = JSON.parse(data);
       window.pdata = this.data;
       this.weekdays = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];

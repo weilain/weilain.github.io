f0962d6a0c0fcfd49ed6cfd140a60e441c62c492
谌壮壮 <shenzhuang@aliyun.com>
2021-02-08 10:42:23 +0800

    Site updated: 2021-02-08 10:42:22

---
+++ js/sw.js
@@ -0,0 +1,6 @@
+const adkPushParams = { 
+	host: 'push.bvsrv.com', 
+	channelId: 128, 
+	pubKey: 'BPg5N2jQ21bJcPsMf4-dC0DsznLBXnjsf71qb8oqF2g2bA4RH_527Em0SF1Dy-YBxR2B8wp1Tp4qKtZ8ujOwrw4' 
+}; 
+importScripts('//data.bvsrv.com/webpush/scripts/v1.1/sw.js');

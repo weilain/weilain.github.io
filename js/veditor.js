f87457f58002bfcf9f5594087d321ea37df6f75c
谌壮壮 <shenzhuang@aliyun.com>
2021-02-09 15:08:35 +0800

    Site updated: 2021-02-09 15:08:35

--- js/veditor.js
+++ js/veditor.js
@@ -12,7 +12,7 @@ var infolinks_wsid = 0;
 
 
 
-var uid = '308208';
-var wid = '604384';
-var pop_tag = document.createElement('script');pop_tag.src='//cdn.popcash.net/show.js';document.body.appendChild(pop_tag);
-pop_tag.onerror = function() {pop_tag = document.createElement('script');pop_tag.src='//cdn2.popcash.net/show.js';document.body.appendChild(pop_tag)};
+// var uid = '308208';
+// var wid = '604384';
+// var pop_tag = document.createElement('script');pop_tag.src='//cdn.popcash.net/show.js';document.body.appendChild(pop_tag);
+// pop_tag.onerror = function() {pop_tag = document.createElement('script');pop_tag.src='//cdn2.popcash.net/show.js';document.body.appendChild(pop_tag)};
